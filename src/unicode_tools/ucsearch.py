bd36d8d60d9c561243b3048a442e2c092baf22a0
Miki Yutani <mkyutani@gmail.com>
2023-01-29 00:50:41 +0900

    Search by block

--- src/unicode_tools/ucsearch.py
+++ src/unicode_tools/ucsearch.py
@@ -18,24 +18,41 @@ def get_code_range(fragment):
         r = int(fragment, 16)
     return r
 
-def search(fragment, by_code=False, short=False):
+def search(fragment, by, short=False):
     with Connection() as conn:
-        with Cursor(conn) as cur:
-            if by_code:
-                code_range = get_code_range(fragment)
-                if type(code_range) is tuple:
-                    cond = f'where code >= {code_range[0]} and code <= {code_range[1]} order by code'
-                else:
-                    cond = f'where code = {code_range}'
+        char_list = []
+        if by == 'code':
+            code_range = get_code_range(fragment)
+            if type(code_range) is tuple:
+                cond = f'where code >= {code_range[0]} and code <= {code_range[1]}'
             else:
-                cond = f'where name like "%{fragment.upper()}%" order by code'
+                cond = f'where code = {code_range}'
             dml = f'select * from char {cond}'
-            cur.execute(dml)
-            for (code, name, char) in cur.fetchall():
-                if short:
-                    print(f'{char}', end='')
-                else:
-                    print(f'{char} {code:X} {name}')
+            with Cursor(conn) as cur:
+                cur.execute(dml)
+                char_list = cur.fetchall()
+        elif by == 'block':
+            dml_block = f'select * from block where name like "%{fragment}%"'
+            with Cursor(conn) as cur_block:
+                cur_block.execute(dml_block)
+                block_list = cur_block.fetchall()
+            for (block_name, min, max) in block_list:
+                dml = f'select * from char where code >= {min} and code <= {max}'
+                with Cursor(conn) as cur:
+                    cur.execute(dml)
+                    char_list.extend(cur.fetchall())
+        else:
+            dml = f'select * from char where name like "%{fragment}%" order by code'
+            with Cursor(conn) as cur:
+                cur.execute(dml)
+                char_list = cur.fetchall()
+
+        char_list.sort(key=lambda x: x[0])
+        for (code, name, char) in char_list:
+            if short:
+                print(f'{char}', end='')
+            else:
+                print(f'{char} {code:X} {name}')
 
 def ucsearch():
     sys.stdin = io.TextIOWrapper(sys.stdin.buffer, encoding="utf-8")
@@ -45,6 +62,7 @@ def ucsearch():
     import argparse
     parser = argparse.ArgumentParser(description='Search unicode characters')
     parser.add_argument('expression', nargs='+', metavar='EXPR', help='expression to search')
+    parser.add_argument('-b', '--block', action='store_true', help='by block name')
     parser.add_argument('-c', '--code', action='store_true', help='by code')
     parser.add_argument('-s', '--short', action='store_true', help='print character only')
     parser.add_argument('-I', '--info', nargs=1, default=[None], help='print character information')
@@ -55,7 +73,14 @@ def ucsearch():
 
     args = parser.parse_args()
 
+    if args.code:
+        by = 'code'
+    elif args.block:
+        by = 'block'
+    else:
+        by = None
+
     for expr in args.expression:
-        search(expr, by_code=args.code, short=args.short)
+        search(expr, by, short=args.short)
 
     return 0
